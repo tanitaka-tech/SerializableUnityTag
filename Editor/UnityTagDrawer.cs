8e50b969eede0c6c06f04ef5514c58b1b4c3d284
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2024-04-22 01:28:27 +0900

    🌱 First Commit

---
+++ Editor/UnityTagDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SerializableUnityTag.Editor
+{
+    // タグ名のプロパティを表示するためのPropertyDrawer
+    [CustomPropertyDrawer(typeof(UnityTag))]
+    public class UnityTagDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var tagNameProperty = property.FindPropertyRelative("_tagName");
+
+            // タグフィールドを表示
+            var tag = EditorGUI.TagField(position, label, tagNameProperty.stringValue);
+
+            // タグ名を反映
+            tagNameProperty.stringValue = tag;
+        }
+    }
+}
---
+++ Runtime/UnityTag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace SerializableUnityTag
+{
+    [Serializable]
+    public struct UnityTag
+    {
+        // タグ名
+        [SerializeField] private string _tagName;
+
+        // タグ名のプロパティ
+        public string Name
+        {
+            get => _tagName;
+            set => _tagName = value;
+        }
+
+        // タグ付けされているかどうか
+        public bool IsTagged => !string.IsNullOrEmpty(_tagName) && _tagName != "Untagged";
+
+        // タグ名の比較
+        public static bool operator ==(UnityTag unityTag, string tagName) => unityTag._tagName == tagName;
+        public static bool operator !=(UnityTag unityTag, string tagName) => unityTag._tagName != tagName;
+        public bool Equals(UnityTag other) => _tagName == other._tagName;
+        public override bool Equals(object obj) => obj is UnityTag other && Equals(other);
+
+        // ハッシュコードの取得
+        public override int GetHashCode() => (_tagName != null ? _tagName.GetHashCode() : 0);
+
+        // 文字列への変換
+        public override string ToString() => _tagName;
+    }
+}
